fde55da42cda282b0ade968dcc13122acfcee35a
Alex <alxnvn@yahoo.com.br>
2022-12-12 15:43:45 -0300

    atualizando texto das imagens com hover

---
+++ script.js
@@ -0,0 +1,19 @@
+let images = document.querySelectorAll(".icons-tech img");
+let texts = document.querySelectorAll(".icons-tech p");
+
+
+for (let image of images) {
+
+    image.addEventListener("mouseover",()=> {
+      
+        // for(let text of texts) {
+
+        //     text.addEventListener("mouseover",() => {
+        //        text.style.backgroundColor = "green";
+        //        text.style.display = "none"
+        //     })
+        // }
+        
+        // image.style.backgroundColor = "green";
+    })
+}
